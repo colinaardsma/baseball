67c75f67167735becc2413cafea6c816cc4af398
Colin Aardsma <colin.aardsma@gmail.com>
2016-10-02 15:59:56 -0500

    added test cases for Batter and WebsitePull; tests do nothing right now

---
+++ Baseball/baseball/baseball/BatterTest.java
@@ -0,0 +1,14 @@
+package baseball;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BatterTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Baseball/baseball/baseball/WebsitePullTest.java
@@ -0,0 +1,14 @@
+package baseball;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WebsitePullTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
